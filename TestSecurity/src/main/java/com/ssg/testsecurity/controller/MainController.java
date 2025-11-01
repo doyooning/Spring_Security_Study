e1a886b0214df70eb0c5b1a5899469854e5b2f55
dynii <dyniiyeyo@naver.com>
2025-11-01 22:46:38 +0900

    Practice 9

--- TestSecurity/src/main/java/com/ssg/testsecurity/controller/MainController.java
+++ TestSecurity/src/main/java/com/ssg/testsecurity/controller/MainController.java
@@ -15,13 +15,16 @@ public class MainController {
 
     @GetMapping("/")
     public String mainP(Model model) {
+        // 아이디 get
         String id = SecurityContextHolder.getContext().getAuthentication().getName();
 
+        // 권한 정보 얻는 로직 -> 권한 필요한 곳에 붙여다 써서 검증하면 됨
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
         Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
         Iterator<? extends GrantedAuthority> iter = authorities.iterator();
         GrantedAuthority auth = iter.next();
+
+        // 역할(권한)정보 get
         String role = auth.getAuthority();
 
         model.addAttribute("id", id);
