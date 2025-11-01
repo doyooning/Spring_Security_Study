72bb554bdbca3b12e4dea1a095dfb934483af4a8
dynii <dyniiyeyo@naver.com>
2025-11-01 17:38:36 +0900

    Practice 4

--- TestSecurity/src/main/java/com/ssg/testsecurity/config/SecurityConfig.java
+++ TestSecurity/src/main/java/com/ssg/testsecurity/config/SecurityConfig.java
@@ -11,14 +11,22 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests( // !!! 상단부터 순차 실행 -> 맨 위에서 permitAll 하면 밑에는 소용없다
-                (auth) -> auth
-                        .requestMatchers("/", "/login").permitAll() // 메인페이지, 로그인페이지는 모두에게
+    public SecurityFilterChain filterChain (HttpSecurity http) throws Exception { // !!! 상단부터 순차 실행 -> 맨 위에서 permitAll 하면 밑에는 소용없다
+        http.authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/", "/login", "/loginProc").permitAll() // 메인페이지, 로그인페이지는 모두에게
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .requestMatchers("/my/**").hasAnyRole("USER", "ADMIN")
                         .anyRequest().authenticated() // 위 요청 외에는 인가된 사용자만
         );
+
+        http.formLogin((auth) -> auth.loginPage("/login") // 로그인 페이지를 연결해줌
+                        .loginProcessingUrl("/loginProc") // 로그인이 완료되면 연결할 페이지
+                        .permitAll() // 로그인되면 permitAll 상태
+
+        );
+        // csrf 설정 - 지금은 비활성화로 (로그인이 안됨)
+        http.csrf((auth) -> auth.disable());
+
         return http.build();
     }
 }
