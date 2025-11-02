85f3695e3e429c134ef9f68466cb7c776638ac5e
dynii <dyniiyeyo@naver.com>
2025-11-02 10:38:41 +0900

    Practice 12

--- TestSecurity/src/main/java/com/ssg/testsecurity/config/SecurityConfig.java
+++ TestSecurity/src/main/java/com/ssg/testsecurity/config/SecurityConfig.java
@@ -4,7 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -45,5 +49,24 @@ public class SecurityConfig {
                     .sessionFixation().changeSessionId());
 
         return http.build();
+
+    }
+    // DB 연동 안되어 있을 때 내장된 유저 정보 만들기
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails user1 = User.builder()
+                .username("admintest")
+                .password(bCryptPasswordEncoder().encode("admin1"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user2 = User.builder()
+                .username("usertest")
+                .password(bCryptPasswordEncoder().encode("user1"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
     }
 }
