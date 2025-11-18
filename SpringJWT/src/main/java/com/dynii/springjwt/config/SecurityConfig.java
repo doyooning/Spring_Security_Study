734f07c668a74e999b541480ed577ee365921342
dynii <dyniiyeyo@naver.com>
2025-11-19 08:21:52 +0900

    Practice 11

--- SpringJWT/src/main/java/com/dynii/springjwt/config/SecurityConfig.java
+++ SpringJWT/src/main/java/com/dynii/springjwt/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.dynii.springjwt.config;
 
+import com.dynii.springjwt.jwt.JWTFilter;
 import com.dynii.springjwt.jwt.JWTUtil;
 import com.dynii.springjwt.jwt.LoginFilter;
 import org.springframework.context.annotation.Bean;
@@ -61,6 +62,10 @@ public class SecurityConfig {
                         .requestMatchers("/login", "/", "/join").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .anyRequest().authenticated());
+
+        //JWTFilter 등록
+        http
+                .addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class);
 
         //필터 추가 LoginFilter()는 인자를 받음 (AuthenticationManager() 메소드에 authenticationConfiguration 객체를 넣어야 함) 따라서 등록 필요
         http
