6c16d90c12a51afde80a19dc8146cf64e867dfb2
dynii <dyniiyeyo@naver.com>
2025-11-24 08:40:52 +0900

    OAuth2Session - Practice 12

--- OAuthSession/src/main/java/com/dynii/oauthsession/config/SecurityConfig.java
+++ OAuthSession/src/main/java/com/dynii/oauthsession/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.dynii.oauthsession.config;
 
+import com.dynii.oauthsession.oauth2.CustomClientRegistrationRepo;
 import com.dynii.oauthsession.service.CustomOAuth2UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,9 +14,11 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final CustomOAuth2UserService customOAuth2UserService;
+    private final CustomClientRegistrationRepo customClientRegistrationRepo;
 
-    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService) {
+    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService, CustomClientRegistrationRepo customClientRegistrationRepo) {
         this.customOAuth2UserService = customOAuth2UserService;
+        this.customClientRegistrationRepo = customClientRegistrationRepo;
     }
 
     @Bean
@@ -38,6 +41,7 @@ public class SecurityConfig {
         http
                 .oauth2Login((oauth2) -> oauth2
                         .loginPage("/login")
+                        .clientRegistrationRepository(customClientRegistrationRepo.clientRegistrationRepository())
                         .userInfoEndpoint((userInfoEndpointConfig) -> userInfoEndpointConfig
                                 .userService(customOAuth2UserService)));
 
