48009e1b0f0b1054bdc95010cb1d6bdcb668ac05
dynii <dyniiyeyo@naver.com>
2025-12-02 20:34:29 +0900

    OAuth2JWT - Practice 15

--- OAuthJWT/src/main/java/com/dynii/oauthjwt/config/SecurityConfig.java
+++ OAuthJWT/src/main/java/com/dynii/oauthjwt/config/SecurityConfig.java
@@ -1,55 +1,37 @@
 package com.dynii.oauthjwt.config;
 
-import com.dynii.oauthjwt.service.CustomOAuth2UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
+import java.util.Collections;
 
-    private final CustomOAuth2UserService customOAuth2UserService;
+@Bean
+public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService) {
+    http
+            .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
 
-        this.customOAuth2UserService = customOAuth2UserService;
-    }
+                @Override
+                public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+                    CorsConfiguration configuration = new CorsConfiguration();
 
-        //csrf disable
-        http
-                .csrf((auth) -> auth.disable());
+                    configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                    configuration.setAllowedMethods(Collections.singletonList("*"));
+                    configuration.setAllowCredentials(true);
+                    configuration.setAllowedHeaders(Collections.singletonList("*"));
+                    configuration.setMaxAge(3600L);
 
-        //From 로그인 방식 disable
-        http
-                .formLogin((auth) -> auth.disable());
+                    configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
+                    configuration.setExposedHeaders(Collections.singletonList("Authorization"));
 
-        //HTTP Basic 인증 방식 disable
-        http
-                .httpBasic((auth) -> auth.disable());
+                    return configuration;
+                }
+            }));
 
-        //oauth2 - Default 설정
-        http
-                .oauth2Login(Customizer.withDefaults());
-
-        //경로별 인가 작업
-        http
-                .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/").permitAll()
-                        .anyRequest().authenticated());
-
-        //세션 설정 : STATELESS
-        http
-                .sessionManagement((session) -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-        return http.build();
-    }
+    return http.build();
 }
