09729dd3c8da03f4ca320fffb5e427f0285acb76
dynii <dyniiyeyo@naver.com>
2025-12-02 20:37:19 +0900

    OAuth2JWT - Practice 15

--- OAuthJWT/src/main/java/com/dynii/oauthjwt/config/SecurityConfig.java
+++ OAuthJWT/src/main/java/com/dynii/oauthjwt/config/SecurityConfig.java
@@ -9,29 +9,32 @@ import org.springframework.web.cors.CorsConfigurationSource;
 
 import java.util.Collections;
 
-@Bean
-public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+public class SecurityConfig {
 
-    http
-            .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-                @Override
-                public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+        http
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
 
-                    CorsConfiguration configuration = new CorsConfiguration();
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
 
-                    configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
-                    configuration.setAllowedMethods(Collections.singletonList("*"));
-                    configuration.setAllowCredentials(true);
-                    configuration.setAllowedHeaders(Collections.singletonList("*"));
-                    configuration.setMaxAge(3600L);
+                        CorsConfiguration configuration = new CorsConfiguration();
 
-                    configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
-                    configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                        configuration.setAllowedMethods(Collections.singletonList("*"));
+                        configuration.setAllowCredentials(true);
+                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+                        configuration.setMaxAge(3600L);
 
-                    return configuration;
-                }
-            }));
+                        configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
+                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
 
-    return http.build();
+                        return configuration;
+                    }
+                }));
+
+        return http.build();
+    }
 }
